a153cad441de8855378ca9fd8f51bba06e0ca517
Yash <74345713+yash244466666@users.noreply.github.com>
2024-03-29 14:40:58 +0600

    Add changes according to review

--- backend/db/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-// mongoose.connect(process.env.URL, () => {
-//     console.log("connected to db")
-// }, e => console.error(e));
-
-mongoose.connect(process.env.URL)
-  .then(() => console.log('Connected to Mongo Successfully!'))
-  .catch(error => console.error(error));
