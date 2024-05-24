e29cda35e48859d82f77a3ee7d29b6220b06ae89
Luncy-May <cheng.li1@wustl.edu>
2024-05-24 15:30:22 +0800

    add commentsin frontend

--- frontend/src/components/GoogleLogin/TestLogin.jsx
+++ frontend/src/components/GoogleLogin/TestLogin.jsx
@@ -3,46 +3,48 @@ import { useGoogleLogin, googleLogout } from '@react-oauth/google';
 import axios from 'axios';
 
 function TestLogin() {
-    const [message, setMessage] = useState("Please Login!");
-    const googleOAuthConfig = {
+    const [message, setMessage] = useState("Please Login!"); // message displayed on the screen (purely text, no functionality)
+    const googleOAuthConfig = { // google client secrets 
         clientId: "123603861190-3ugdemkftujm734b9kc10escfbi8qp92.apps.googleusercontent.com",
         redirectUri: "http://localhost:5173"
     };
 
-    const [profile, setProfile] = useState(null);
-    const [showSuccess, setShowSuccess] = useState(false);
+    const [profile, setProfile] = useState(null); // to keep track of user's credential information
+    const [showSuccess, setShowSuccess] = useState(false); // message displayed on the screen (purely text, no functionality)
 
-    const login = useGoogleLogin({
+    const login = useGoogleLogin({ // Functionality for the Google Login Button 
         clientId: googleOAuthConfig.clientId,
         redirectUri: googleOAuthConfig.redirectUri,
         onSuccess: async (response) => { // if the system operates successfully, retrieve credential information from Google OAuth Service
+            // accepting response from Google Cloud Service Server
             try {
-                console.log('Response from server:', response);
-                
+                console.log('Response from server:', response); // response: json
+                // response contains: access_token, token_type, expires_in, scope, authuser, prompt
 
-                const res = await axios.post('http://localhost:3000/auth/google', response );
-                if (res.data.token) {
+                const res = await axios.post('http://localhost:3000/auth/google', response ); // fetch result from server
+                if (res.data.token) { // if the server successfully returns a response 
                     document.cookie = `jwt=${res.data.token}; path=/; HttpOnly`;
-                    setMessage("Welcome, " + res.data.user.name);
-                    setProfile(res.data.user);
-                    setShowSuccess(true);
-                    setTimeout(() => setShowSuccess(false), 2000);
-                }
-            } catch (error) {
+                    setMessage("Welcome, " + res.data.user.name); // changed text
+                    setProfile(res.data.user); // change profile 
+                    setShowSuccess(true); // change text
+                    setTimeout(() => setShowSuccess(false), 2000); // set automatic log-out time frame
+                } 
+            } catch (error) { // the server returns an error 
                 console.log('Login Failed:', error);
             }
         },
         onError: (error) => console.log('Login Failed:', error) // return an error message if there is an internal error in the system
     });
 
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-        document.cookie = 'jwt=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    const logOut = () => { // log out functionality of the button 
+        googleLogout(); // call a built-in functionality to log out
+        setProfile(null); // set profile to null 
+        document.cookie = 'jwt=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'; // update the cookies and jwt to none
     };
 
     return (
-        <div>
+        <div> 
+            {/* These are content displayed on the screen  */}
             <h1>{message}</h1>
             {profile ? (
                 <div>
